a2ae22c03638858c86f923824202f4208be7716a
calkia <kcy.1288@gmail.com>
2025-02-28 10:30:10 -0500

    Update TempConverter.java

--- TempConverter.java
+++ TempConverter.java
@@ -6,6 +6,7 @@ public class TempConverter {
     public static void main(String[] args) {
     double Fahrenheit;
     double Celsius;
+    
     Scanner input = new Scanner(System.in);
     System.out.println("Enter Fahrenheit temp: ");
     Fahrenheit = input.nextDouble();
